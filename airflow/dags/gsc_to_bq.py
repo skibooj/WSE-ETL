2b710b946ddc214643c14fbf9f78e152ff225115
skibooj <ssebastian156@gmail.com>
2022-03-31 19:30:14 +0200

    update

--- airflow/dags/gsc_to_bq.py
+++ airflow/dags/gsc_to_bq.py
@@ -1,27 +1,30 @@
+from datetime import datetime
 import os
-import logging
 
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator, BigQueryInsertJobOperator
+from airflow.providers.google.cloud.operators.bigquery import (
+    BigQueryCreateExternalTableOperator,
+    BigQueryInsertJobOperator,
+)
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
-TODAY_DATE = '{{ execution_date.strftime(\'%d-%m-%Y\') }}'
-SEC_TYPE = '10'
+TODAY_DATE = "{{ execution_date.strftime('%d-%m-%Y') }}"
+SEC_TYPE = "10"
 PATH_TO_LOCAL_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-PATH_TEMPLATE_PARQUET =f"{PATH_TO_LOCAL_HOME}/{TODAY_DATE}.parquet"
-PATH_TEMPLATE_GSC = f"stock/wse/shares/{{{{ execution_date.strftime(\'%Y\') }}}}/{{{{ execution_date.strftime(\'%m\') }}}}/wse_shares_{TODAY_DATE}.parquet"
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'wse_data_all')
+PATH_TEMPLATE_PARQUET = f"{PATH_TO_LOCAL_HOME}/{TODAY_DATE}.parquet"
+PATH_TEMPLATE_GSC = f"stock/wse/shares/{{{{ execution_date.strftime('%Y') }}}}/{{{{ execution_date.strftime('%m') }}}}/wse_shares_{TODAY_DATE}.parquet"
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "wse_data_all")
 
 DATASET = "wse-shares"
 INPUT_FILETYPE = "parquet"
 
 default_args = {
     "owner": "airflow",
-    "start_date": days_ago(1),
+    "start_date": datetime,
     "depends_on_past": False,
     "retries": 1,
 }
@@ -33,9 +36,8 @@ with DAG(
     default_args=default_args,
     catchup=False,
     max_active_runs=1,
-    tags=['wse'],
+    tags=["wse"],
 ) as dag:
-
 
     bigquery_external_table_task = BigQueryCreateExternalTableOperator(
         task_id=f"bq_wse_shares_external_table_task",
@@ -48,9 +50,7 @@ with DAG(
             "externalDataConfiguration": {
                 "autodetect": "True",
                 "sourceFormat": f"{INPUT_FILETYPE.upper()}",
-                "sourceUris": [f"gs://{BUCKET}/stock/*"], 
+                "sourceUris": [f"gs://{BUCKET}/stock/*"],
             },
         },
     )
-
-bigquery_external_table_task 
