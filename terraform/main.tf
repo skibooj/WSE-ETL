433aff496c2acae85cdc94fdfe1fe2fbd577adb6
Sebastian Skiba <sebastianskiba156@gmail.com>
2022-04-04 15:24:52 +0200

    update main.tf
    remove the part that creates vm instances

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,24 +38,4 @@ resource "google_bigquery_dataset" "dataset" {
   project    = var.project
   location   = var.region
 }
-
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "wse-vm"
-  machine_type = "f1-micro"
-  zone = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
- network_interface {
-    # A default network is created for all GCP projects
-    network = "default"
-    access_config {
-    }
-  }
-}
-
   
