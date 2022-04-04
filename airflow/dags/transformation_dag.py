b13bacb96f73cab8a4683c44ddb0f0f909307aa2
skibooj <ssebastian156@gmail.com>
2022-04-04 21:07:36 +0200

    update

--- airflow/dags/transformation_dag.py
+++ airflow/dags/transformation_dag.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 import os
 
+from airflow.operators.bash import BashOperator
+
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryCreateExternalTableOperator,
-    BigQueryInsertJobOperator,
-)
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
@@ -24,33 +22,22 @@ INPUT_FILETYPE = "parquet"
 
 default_args = {
     "owner": "airflow",
-    "start_date": datetime(2019,1,1),
+    "start_date": days_ago(1),
     "depends_on_past": False,
     "retries": 1,
 }
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
-    dag_id="gcs_to_bq_dag",
-    schedule_interval="0 19 * * 1-5",
+    dag_id="transformation_dag",
+    schedule_interval="0 21 * * 1-5",
     default_args=default_args,
     catchup=False,
     max_active_runs=1,
     tags=["wse"],
 ) as dag:
 
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id=f"bq_wse_shares_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": f"wse_shares_external_table",
-            },
-            "externalDataConfiguration": {
-                "autodetect": "True",
-                "sourceFormat": f"{INPUT_FILETYPE.upper()}",
-                "sourceUris": [f"gs://{BUCKET}/stock/*"],
-            },
-        },
+    execute_dbt_task = BashOperator(
+        task_id="dbt_streamify_run",
+        bash_command="cd /dbt && dbt build --profiles-dir . --target prod",
     )
